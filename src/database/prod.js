69cb53f505a3ef8a43fd77c60cbbd49632327973
Christian Kodehode <christian404.kodehode@gmail.com>
2025-12-19 15:04:23 +0100

    updating to use sqlite to avoid admin restrictions

---
+++ src/database/prod.js
@@ -0,0 +1,11 @@
+const db = requrie("../config/prod.js");
+
+db.prepare(
+  `CREATE TABLE IF NOT EXISTS users (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+email TEXT UNIQUE NOT null,
+password TEXT NOT null,
+created_at DATETIME DEFAULT CURRENT_TIMESTAMP 
+)
+`
+).run();
