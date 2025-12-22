51fa22fe8e04108ceacf52697265c5d5e0b5a82b
Christian Kodehode <christian404.kodehode@gmail.com>
2025-12-22 14:37:50 +0100

    app.js updated

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 const express = require("express");
-require("./database/prod.js");
-
-const authRoutes = require("./routes/auth.routes");
-const errorMiddleware = require("./middleware/error.middleware");
-
 const app = express();
 
-// Parsing JSON request
+const authRoutes = require("./routes/auth.routes.js");
+const userRoutes = require("./routes/user.routes.js");
+
 app.use(express.json());
 
-// Routes for auth
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
-// We handle errors here
-app.use(errorMiddleware);
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500).json({
+    error: err.messsage || "Server error, or the hamster is out of peanuts.",
+  });
+});
 
 module.exports = app;
