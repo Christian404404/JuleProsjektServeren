411239e91e15c97b424802e3aa375a68eadf92c9
Christian Kodehode <christian404.kodehode@gmail.com>
2025-12-22 14:51:12 +0100

    app.js updated with information about a template folder existing, for ease of development and testing

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
+// templates are gitignored, access to the templates is dev only
+// this is just for ease of development and testing when I add more features
 const express = require("express");
 const app = express();
 
 const authRoutes = require("./routes/auth.routes.js");
 const userRoutes = require("./routes/user.routes.js");
+// const templateRoutes = require('./routes/template.routes');
 
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+// app.use('/api/template', templateRoutes);
 
 app.use((err, req, res, next) => {
   res.status(err.statusCode || 500).json({
