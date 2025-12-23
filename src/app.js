62e3bcff31295ebcc5ef36b61a0b1d62e6a0b534
Christian Kodehode <christian404.kodehode@gmail.com>
2025-12-23 12:47:09 +0100

    cors added to connect back end to front end

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 // templates are gitignored, access to the templates is dev only
 // this is just for ease of development and testing when I add more features
+
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Current frontend, adjust as needed later.
+    credentials: true,
+  })
+);
+
+app.use(express.json());
 
 const authRoutes = require("./routes/auth.routes.js");
 const userRoutes = require("./routes/user.routes.js");
 // const templateRoutes = require('./routes/template.routes');
-
-app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
@@ -15,7 +25,9 @@ app.use("/api/user", userRoutes);
 
 app.use((err, req, res, next) => {
   res.status(err.statusCode || 500).json({
-    error: err.messsage || "Server error, or the hamster is out of peanuts.",
+    error:
+      err.message ||
+      "Server error, developer Catgirl might be upset and is intentionally blocking your request/access.",
   });
 });
 
